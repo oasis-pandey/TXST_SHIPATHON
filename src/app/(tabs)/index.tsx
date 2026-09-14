f0dc7691493b8da087c46e5b5933a00339289bbe
Oasis Pandey <fno21@txstate.edu>
2026-09-14 13:48:49 -0500

    Keep discover tab screen focused

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import { DiscoverFeature } from '@/matching/discover/DiscoverFeature';
 
 export default function DiscoverScreen() {
-  return <DiscoverFeature />;
+    return <DiscoverFeature />;
 }
